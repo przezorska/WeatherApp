d053167cad93138ec8158680bb6ea44795beaac7
Eunice <eunika.przezorska@gmail.com>
2018-09-26 16:06:18 +0200

    Spaces and new class added

--- weather-app/src/components/weather.js
+++ weather-app/src/components/weather.js
@@ -3,27 +3,27 @@ import React from 'react';
 const Weather = props => (
   <div className='weather__info'>
     { 
-      props.city && props.country && <p className='weather__key'>Lokalizacja: 
-      <span>{ props.city }, { props.country }</span>
+      props.city && props.country && <p className='weather__key'>Lokalizacja:
+      <span className='weather__value'> { props.city }, { props.country }</span>
       </p>
     }
     { 
-      props.temperature && <p className='weather__key'>Temperatura: 
-      <span>{ props.temperature }</span>
+      props.temperature && <p className='weather__key'>Temperatura:
+      <span className='weather__value'> { props.temperature }</span>
       </p>
     }
     { 
       props.humidity && <p className='weather__key'>Wilgotność: 
-      <span>{ props.humidity }</span>
+      <span className='weather__value'> { props.humidity }</span>
       </p>
     }
     { 
       props.description && <p className='weather__key'>Warunki: 
-      <span>{ props.description }</span>
+      <span className='weather__value'> { props.description }</span>
       </p>
     }
     { 
-      props.error && <p>{ props.error }</p> 
+      props.error && <p className='weather__error'>{ props.error }</p> 
     }
   </div>
 );
